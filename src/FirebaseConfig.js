db5e33e106c0c7108284dbff6f72b7a0a6288568
akshitaggarwal100 <akshitaggarwal64@gmail.com>
2023-01-17 20:36:34 +0530

    deleted comments

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY , //'AIzaSyBAEx_tpsL_HRwnAhzC8E_xtFVjtaoY-4M',
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN, //'todo-list-24124.firebaseapp.com',
-    projectId: process.env.REACT_APP_PROJECT_ID, //'todo-list-24124',
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKETREACT_APP_STORAGE_BUCKET, //'todo-list-24124.appspot.com',
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID, //'770683609707',
-    appId: process.env.REACT_APP_APP_ID //'1:770683609707:web:43fa0d596b061d8cc2472e'
+    apiKey: process.env.REACT_APP_API_KEY , 
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID, 
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKETREACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID 
 }
 
 const app = initializeApp(firebaseConfig)
